39cd7147b7fefe5ab213369445b1ba6d1c74e580
Yippeekkyy <arcticmonkey777@yandex.ru>
2022-10-26 17:25:57 +0200

    lala

---
+++ SB12.5/UsingAssert/UsingAssert/UsingAssert.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cassert>
+
+float travelTime(float distance, float speed) 
+{
+    assert(speed > 0);
+    float time = distance / speed;
+    float hours = time / 60;
+    int minutes = 0;
+    std::cout << speed;
+
+    return 0;
+}
+
+int main()
+{
+    travelTime(100, 0);
+
+    return 0;
+}
+
+
