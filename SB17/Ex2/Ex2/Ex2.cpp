a4486a18988d58229f99a2711dd249aa7b42ca24
Yippeekkyy <arcticmonkey777@yandex.ru>
2022-11-07 17:38:59 +0200

    ex2

---
+++ SB17/Ex2/Ex2/Ex2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void rev(int* x , int y) {
+    for (int i = 0, buff = 0; i < y / 2 ; ++i) {
+        buff = *(x + (y - 1 - i));
+        *(x + (y - 1 - i)) = *(x + i);
+        *(x + i) = buff;
+        
+    }
+    
+    
+}
+
+int main()
+{
+    int arr[] = { 10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100 , 110};
+    rev(arr , 11);
+
+    for (int i = 0; i < sizeof arr / sizeof arr[0]; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    
+}
+
---
+++ SB17/ukazatel/ukazatel/ukazatel.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+void swaper(int* x , int* y ) {
+    int buff = *x;
+    *x = *y;
+    *y = buff;
+    
+
+}
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    swaper(&a, &b);
+    std::cout << a << b;
+}
+
